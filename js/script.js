24891c99e34530839def99c4dc3ddd5ee0e74117
Thezero <67761713+Thezero27@users.noreply.github.com>
2023-06-23 19:01:44 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function speak() {
         return text.includes(valor);
     });
     //Respuestas
-    var nombreIA = "Mi nombre es Merly y me crearon para estar a tu lado y poder ayudarte en lo que necesites"
+    var nombreIA = "Mi nombre es Shayla y me crearon para estar a tu lado y poder ayudarte en lo que necesites"
     var fue = "No entiendo lo que me quieres decir, aun estoy en desarrollo no logro entender muchas cosas"
     //var respuestax = "Mi nombre es Merly y me crearon para estar a tu lado y poder ayudarte en lo que necesites,No entiendo lo que me quieres decir, aun estoy en desarrollo no logro entender muchas cosas"
 
