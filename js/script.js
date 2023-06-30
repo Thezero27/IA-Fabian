b57227cdb470dec023c28f4d2c6a71f9e71e1137
Thezero <67761713+Thezero27@users.noreply.github.com>
2023-06-30 16:25:31 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+// Obtener la voz femenina
 function getFemaleVoice() {
-    var voices = window.speechSynthesis.getVoices();
+    var voices = speechSynthesis.getVoices();
     // Buscar la voz específica
     var voice = voices.find(function (voice) {
         return voice.name === "Microsoft Sabina - Spanish (Mexico)";
@@ -9,27 +10,24 @@ function getFemaleVoice() {
 
 
 
-//---------API DE CLIMA-----------------
+// API de Clima
 const apiKey = '0b15df09486e5a9db2cf2f9a615cee08';
 const ciudad = 'Tingo Maria';
 var clima;
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    const climaActual = data;
-    const temperaturaKelvin = climaActual.main.temp;
-    const temperaturaCelsius = temperaturaKelvin - 273.15;
-    clima = "El clima en Tingo Maria es " + temperaturaCelsius.toFixed(2) + "°C";
-    
-    // Aquí puedes realizar otras operaciones con el clima actual si es necesario
-  })
-  .catch(error => {
-    console.error('Error al obtener los datos del clima:', error);
-  });
+    .then(response => response.json())
+    .then(data => {
+        const climaActual = data;
+        const temperaturaKelvin = climaActual.main.temp;
+        const temperaturaCelsius = temperaturaKelvin - 273.15;
+        clima = "El clima en Tingo Maria es " + temperaturaCelsius.toFixed(2) + "°C";
 
-
-
+        // Aquí puedes realizar otras operaciones con el clima actual si es necesario
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos del clima:', error);
+    });
 
 var btnEnviar = document.getElementById("btnE");
 btnEnviar.onclick = function () {
@@ -40,7 +38,7 @@ btnEnviar.onclick = function () {
     var valorEncontrado = valores.find(function (valor) {
         return text.includes(valor);
     });
-    
+
     // PREGUNTA SI QUIERE SER TU AMIGA   
     var pregAmiga = "puedes ser mi amiga,quiero ser tu amiga";
     var Amigos = pregAmiga.split(",");
@@ -55,13 +53,20 @@ btnEnviar.onclick = function () {
         return text.includes(valor);
     });
 
-
     // PREGUNTA DEL CLIMA--------   
     var pregClim = "como esta el clima,como va el clima,va llover";
     var Climax = pregClim.split(",");
     var valorClime = Climax.find(function (valor) {
         return text.includes(valor);
     });
+
+    // REDIRECCIONAR A WEBSITE DE SEARCH-DNI
+    var pregDni = "buscar dni,hallar dni,dni por nombre,consultar dni";
+    var Dnisearch = pregDni.split(",");
+    var valorDni = Dnisearch.find(function (valor) {
+        return text.includes(valor);
+    });
+
 
 
 
@@ -76,9 +81,8 @@ btnEnviar.onclick = function () {
     var nombreIA = "Mi nombre es Merly y me crearon para estar a tu lado y poder ayudarte en lo que necesites";
     var friends = "No se diga más, a partir de ahora, siendo " + horaExacta + ", me declaro tu amiga incondicional";
     var fue = "No entiendo lo que me quieres decir, aún estoy en desarrollo y no logro entender muchas cosas";
+    var mantenimientox = "Servicio no disponible por temas de mantenimiento Fabian esta mejorando dicho servicio, puedes revisar en la tabla los diferentes comandos";
     var peli = "Por el momento, la película que te recomiendo para ver en pareja es 'A todos los chicos de los que me enamoré' y 'Están los besos'";
-    //var clima = "El clima en Tingo Maria es " + climaActual.main.temp + "°C";
-
 
     var speechSynthesis = window.speechSynthesis;
     var speechUtterance = new SpeechSynthesisUtterance();
@@ -94,6 +98,8 @@ btnEnviar.onclick = function () {
         speechUtterance.text = peli;
     } else if (valorClime) {
         speechUtterance.text = clima;
+    } else if (valorDni) {
+        speechUtterance.text = mantenimientox;
     } else {
         speechUtterance.text = fue;
     }
